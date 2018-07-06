520471e665db12a8db59e9991635681d32de92f9
Max <maxwell.rubin.willard@gmail.com>
2018-07-06 09:42:44 -0700

    Adding example lambdas, buildspec and template for pipeline testing.

---
+++ HelloWorld_tst.py
@@ -0,0 +1,11 @@
+mport unittest
+from HelloWorld import HelloWorld
+
+class MyTestCase(unittest.TestCase):
+  def test_default_greeting_set(self):
+    #hw = HelloWorld()
+    #self.assertEqual(hw.message, 'Hello world!')
+    pass
+
+if __name__ == '__main__':
+  unittest.main()
