99d75ed3682eeeba267e71e66b8acd553660c475
Max <maxwell.rubin.willard@gmail.com>
2018-07-06 16:38:08 -0700

    Switch to post and simplify start code.

--- returnS3Buckets.py
+++ returnS3Buckets.py
@@ -1,7 +1,3 @@
 def handler(event, context):
     print(event)
-    message = 'Hello {} {}!'.format(event['first_name'],
-                                    event['last_name'])
-    return {
-        'message' : message
-    }
+    return event
